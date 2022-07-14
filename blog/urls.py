16d03527690b6d9850c8e2232fd91beb192edc98
sacar99 <sagarguragain12@gmail.com>
2022-07-14 10:12:01 +0545

    view, template, and url for post_detail added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 
 app_name = "blog"
 urlpatterns = [
+    path("topic/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home"),
 ]
