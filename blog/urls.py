7120e7eb61fbc05220a0ece3da238f7ae7d762d3
sacar99 <sagarguragain12@gmail.com>
2022-07-15 19:23:31 +0545

    post new template written

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 
 app_name = "blog"
 urlpatterns = [
-    path("topic/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home"),
+    path("topic/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="new")
 ]
