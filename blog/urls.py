aefe5154064c792b153072142f9071aaaae8dbfa
sacar99 <sagarguragain12@gmail.com>
2022-07-17 06:52:10 +0545

    postdeleteview, urls and templates updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 
 app_name = "blog"
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("topic/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-    path("post/new/", BlogCreateView.as_view(), name="new")
+    path("post/new/", BlogCreateView.as_view(), name="new"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name='post_edit'),
+    path("post/<int:pk>/del/", BlogDeleteView.as_view(), name="del"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 #from django.shortcuts import render
+# from audioop import reverse
+# from tkinter.ttk import LabeledScale
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 class BlogListView(ListView):
@@ -15,4 +18,17 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "body", "author"]
-    
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_del.html"
+    success_url = reverse_lazy("blog:home")
+    # @staticmethod
+    # def options_to_confirm(request):
+    #     if request.POST == "Confirm":
+    #         success_url = reverse("blog:new")
